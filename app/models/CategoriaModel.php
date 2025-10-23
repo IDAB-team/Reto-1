35f3d58a018b1c97a3bd64230c0489f54333d66a
ikeerhg <80524650+ikeerhg@users.noreply.github.com>
2025-10-23 13:16:02 +0200

    subir Anuncio terminado
    subir Anuncio terminado

--- app/models/CategoriaModel.php
+++ app/models/CategoriaModel.php
@@ -2,11 +2,22 @@
 require_once __DIR__ . '/Database.php';
 
 class CategoriaModel {
+    
     public static function getAll() {
         $db = Database::getConnection();
         $sqlQuery = "SELECT * FROM Categorias";
         $stmt = $db->prepare($sqlQuery);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public static function devolverIdCategoria($nombre){
+        $dba= Database::getConnection();
+        $stmt = $dba ->prepare("SELECT ID_Categoria FROM categorias WHERE Nombre = :nombre");
+        $stmt -> execute(["nombre" => $nombre]);
+        $resultado = $stmt ->fetch(PDO::FETCH_ASSOC);
+        if ($resultado) {
+            return $resultado['ID_Categoria'];
+        }
     }
 }

ccf56c1b93a6ddb2de7be3eecb0b38c0cf623817
BryanAlzamora <bryandave.alzamora@ikasle.egibide.org>
2025-10-27 09:28:56 +0100

    avance

--- app/views/filtrado.view.php
+++ app/views/filtrado.view.php
@@ -51,37 +51,33 @@
     <div class="filtradoAnuncioListado">
     <?php foreach ($listaAnuncios as $anuncio): ?>
         <div class="filtradoAnunciosAnuncioCard">
-        <div class="filtradoAnunciosImagen">
+          <div class="filtradoAnunciosImagen">
+            <a href="index.php?controller=AnuncioController&accion=getAnuncioById&idAnuncio=<?= $anuncio->ID_Anuncio ?>">
             <img src="./<?= $anuncio->Url_imagen ?>" alt="<?= $anuncio->nombreAnuncio ?>">
-        </div>
-        <div class="filtradoAnunciosInfo">
-            <h4><?= $anuncio->nombreAnuncio ?></h4>
-            <h5><?= $anuncio->usernameAnuncio ?></h5>
-            <p><?= $anuncio->descripcionAnuncio ?></p>
-            <p class="filtradoFechaAnuncio"><?= date('d/m/Y', strtotime($anuncio->Fecha_pub)) ?></p>
-            <div class="filtradoAnunciosPrecio">
-            <p><?= $anuncio->precioAnuncio ?> €</p>
+            </a>
+          </div>
+            <div class="filtradoAnunciosInfo">
+                <h4><?= $anuncio->nombreAnuncio ?></h4>
+                <h5><?= $anuncio->usernameAnuncio ?></h5>
+                <p><?= $anuncio->descripcionAnuncio ?></p>
+                <p class="filtradoFechaAnuncio"><?= date('d/m/Y', strtotime($anuncio->Fecha_pub)) ?></p>
+                  <div class="filtradoAnunciosPrecio">
+                    <p><?= $anuncio->precioAnuncio ?> €</p>
             </div>
 
-            <!-- Esto marca los favoritos ya añadidos y aplica la clase para que se vean en rojo.-->
-            <?php if (!empty($user)): ?>
-            <a href="index.php?controller=FavoritosController&accion=existeFavorito&ID_Anuncio=<?= $anuncio->ID_Anuncio ?>" 
-              class="favoritoToggle <?= in_array($anuncio->ID_Anuncio, $favoritos ?? []) ? 'favoritoActivo' : '' ?>">
-
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d='M9 10.5L11 12.5L15.5 8M19 21V7.8C19 6.11984 19 5.27976 18.673 4.63803C18.3854 4.07354 17.9265 3.6146 17.362 3.32698C16.7202 3 15.8802 3 14.2 3H9.8C8.11984 3 7.27976 3 6.63803 3.32698C6.07354 3.6146 5.6146 4.07354 5.32698 4.63803C5 5.27976 5 6.11984 5 7.8V21L12 17L19 21Z' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/>
-              </svg>
-              
-            </a>
-
-
-            <?php endif; ?>
-
-
-            
+                <!-- Esto marca los favoritos ya añadidos y aplica la clase para que se vean en rojo.-->
+             <?php if (!empty($user)): ?>
+              <a href="index.php?controller=FavoritosController&accion=existeFavorito&ID_Anuncio=<?= $anuncio->ID_Anuncio ?>" 
+                class="favoritoToggle <?= in_array($anuncio->ID_Anuncio, $favoritos ?? []) ? 'favoritoActivo' : '' ?>">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d='M9 10.5L11 12.5L15.5 8M19 21V7.8C19 6.11984 19 5.27976 18.673 4.63803C18.3854 4.07354 17.9265 3.6146 17.362 3.32698C16.7202 3 15.8802 3 14.2 3H9.8C8.11984 3 7.27976 3 6.63803 3.32698C6.07354 3.6146 5.6146 4.07354 5.32698 4.63803C5 5.27976 5 6.11984 5 7.8V21L12 17L19 21Z' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/>
+                </svg>
+                </a>         
+              <?php endif; ?>
         </div>
-        </div>
+      </div>      
     <?php endforeach; ?>
+    
     </div>
 
     <!-- Paginación (estática por ahora) -->

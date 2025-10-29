39174b6abf062774acb690f3676d5512365752b6
BryanAlzamora <bryandave.alzamora@ikasle.egibide.org>
2025-10-29 19:10:34 +0100

    Favorito.js y filtrado.js arrreglado
    Modificacion del innerHTML para que filtro sea dinamico

--- app/assets/scripts/filtrado.js
+++ app/assets/scripts/filtrado.js
@@ -29,34 +29,54 @@ function renderAnuncios(anuncios) {
     return;
   }
 
-  anuncios.forEach(anuncio => {
-    const box = document.createElement('div');
-    box.classList.add('filtradoAnunciosAnuncioCard');
-    box.innerHTML = `
-      <div class="filtradoAnunciosImagen">
+anuncios.forEach(anuncio => {
+  const box = document.createElement('div');
+  box.classList.add('filtradoAnunciosAnuncioCard');
+
+  box.innerHTML = `
+    <div class="filtradoAnunciosImagen">
+      <a href="index.php?controller=AnuncioController&accion=getAnuncioById&idAnuncio=${anuncio.ID_Anuncio}">
         <img src="./${anuncio.Url_imagen}" alt="${anuncio.nombreAnuncio}">
-      </div>
-      <div class="filtradoAnunciosInfo">
-        <h4>${anuncio.nombreAnuncio}</h4>
-        <h5>${anuncio.usernameAnuncio}</h5>
-        <p>${anuncio.descripcionAnuncio}</p>
-        <p class="filtradoFechaAnuncio">${new Date(anuncio.fechaAnuncio).toLocaleDateString()}</p>
+      </a>
+    </div>
+
+    <div class="filtradoFechaAnuncio">
+      <p>${new Date(anuncio.fechaAnuncio).toLocaleDateString()}</p>
+    </div>
+
+    <div class="filtradoAnunciosInfo">
+      <div class="filtradoInfoHeader">
+        <div class="filtradoInfoTextos">
+          <h4>${anuncio.nombreAnuncio}</h4>
+          <h5>${anuncio.usernameAnuncio}</h5>
+        </div>
         <div class="filtradoAnunciosPrecio">
           <p>${anuncio.precioAnuncio} €</p>
         </div>
-        ${usuarioLogueado ? `
+      </div>
+
+      <p class="filtradoDescripcion">${anuncio.descripcionAnuncio}</p>
+
+      ${usuarioLogueado ? `
+      <div class="filtradoAnuncioFavorito">
+        <p>Favorito</p>
         <a href="index.php?controller=FavoritosController&accion=existeFavorito&ID_Anuncio=${anuncio.ID_Anuncio}" 
            class="favoritoToggle ${favoritosUsuario.includes(anuncio.ID_Anuncio) ? 'favoritoActivo' : ''}">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-            <path d='M9 10.5L11 12.5L15.5 8M19 21V7.8C19 6.11984 19 5.27976 18.673 4.63803C18.3854 4.07354 17.9265 3.6146 17.362 3.32698C16.7202 3 15.8802 3 14.2 3H9.8C8.11984 3 7.27976 3 6.63803 3.32698C6.07354 3.6146 5.6146 4.07354 5.32698 4.63803C5 5.27976 5 6.11984 5 7.8V21L12 17L19 21Z' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/>
+          <svg width="22" height="22" viewBox="0 0 24 24" fill="none">
+            <path d='M9 10.5L11 12.5L15.5 8M19 21V7.8C19 6.11984 19 5.27976 18.673 4.63803C18.3854 4.07354 17.9265 3.6146 17.362 3.32698C16.7202 3 15.8802 3 14.2 3H9.8C8.11984 3 7.27976 3 6.63803 3.32698C6.07354 3.6146 5.6146 4.07354 5.32698 4.63803C5 5.27976 5 6.11984 5 7.8V21L12 17L19 21Z' 
+                  stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/>
           </svg>
-        </a>` : ''}
-      </div>
-    `;
-    contenedor.appendChild(box);
-  });
+        </a>
+      </div>` : ''}
+    </div>
+  `;
 
-  if (usuarioLogueado) toggleFavorito();
+  contenedor.appendChild(box);
+});
+
+// Inicializa el toggle de favoritos si hay usuario logueado
+if (usuarioLogueado) toggleFavorito();
+
 }
 
 // Busqueda
